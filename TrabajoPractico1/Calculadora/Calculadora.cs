ff77b28316e4a99aba51347224cb5e0cada7536c
Santiago Leonardi <Santiago.t.leonardi@gmail.com>
2022-04-16 17:22:25 -0300

    Agregando Documentacion

--- TrabajoPractico1/Calculadora/Calculadora.cs
+++ TrabajoPractico1/Calculadora/Calculadora.cs
@@ -4,6 +4,13 @@ namespace Biblioteca
 {
     public static class Calculadora
     {
+        /// <summary>
+        /// Opera los valores del tipo operador dependiendo del operacion elegida
+        /// </summary>
+        /// <param name="num1">Primero parametro del tipo operador</param>
+        /// <param name="num2">Segundo parametro del tipo operador</param>
+        /// <param name="operador"></param>
+        /// <returns>El resultado de la operacion elegia del tipo de dato double</returns>
         public static double Operar(Operando num1, Operando num2, char operador)
         {
             char opcion = ValidarOperador(operador);
@@ -30,6 +37,12 @@ namespace Biblioteca
 
             return retorno;
         }
+
+        /// <summary>
+        /// Valida que el operador sea valido en caso contrario asigna "+"
+        /// </summary>
+        /// <param name="operador">Operador del tipo char a validar</param>
+        /// <returns>Retorno del operador validado</returns>
         private static char ValidarOperador(char operador)
         {
             if (operador == '+' || operador == '-' || operador == '/' || operador == '*')
--- TrabajoPractico1/InterfazGrafica/FormCalculadora.cs
+++ TrabajoPractico1/InterfazGrafica/FormCalculadora.cs
@@ -14,13 +14,20 @@ namespace InterfazGrafica
     public partial class FormCalculadora : Form
     {
 
+        /// <summary>
+        /// Contrucor de la clase formCalculadora
+        /// </summary>
         public FormCalculadora()
         {
             InitializeComponent();
             btnConverirBinario.Enabled = false;
             btnConvertirDecimal.Enabled = false;
         }
-
+        /// <summary>
+        /// Llama al metodo operar al apretar el boton 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Operar_Click(object sender, EventArgs e)
         {
             double resultado;
@@ -40,21 +47,41 @@ namespace InterfazGrafica
             btnConvertirDecimal.Enabled = false;
 
         }
-
+        /// <summary>
+        /// Llama al metodo limpiar al apretar el boton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
             this.Limpiar();
         }
 
+        /// <summary>
+        /// LLama al metodo limpiar cuando se cargar el formulario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Limpiar();
         }
 
+        /// <summary>
+        /// LLama al medotod close al apretar el boton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCerrar_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        /// <summary>
+        /// Llama a la funcion convertir decimal a binario de la clase Operando al apretar el boton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnConverirBinario_Click(object sender, EventArgs e)
         {
             lblResultado.Text = Operando.DecimalBinario(lblResultado.Text);
@@ -65,6 +92,11 @@ namespace InterfazGrafica
                 btnConvertirDecimal.Enabled = true;
             }
         }
+        /// <summary>
+        /// LLama a la funcion convetir binario a decimal de la clase Operando al apretar el boton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnConvertirDecimal_Click(object sender, EventArgs e)
         {
             lblResultado.Text = Operando.BinarioDecimal(lblResultado.Text);
@@ -72,6 +104,11 @@ namespace InterfazGrafica
             btnConvertirDecimal.Enabled = false;
         }
 
+        /// <summary>
+        /// Pregunta si realmente quieres salir cuando se esta cerrando el formulario.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCerrar_FormClosing(object sender, FormClosingEventArgs e)
         {
            
@@ -82,6 +119,13 @@ namespace InterfazGrafica
             
         }
 
+        /// <summary>
+        /// Realiza y crea los elemento para realizar los calculo segundo los parametros ingresados
+        /// </summary>
+        /// <param name="numero1">numero al operar del tipo string</param>
+        /// <param name="numero2">numero al operar del tiopp string</param>
+        /// <param name="operador">operador para realizar la operacion</param>
+        /// <returns>Resultado de la operacion del tipo double</returns>
         private static double operar(string numero1, string numero2, string operador)
         {
             double resultado;
@@ -99,6 +143,9 @@ namespace InterfazGrafica
 
             return resultado;
         }
+        /// <summary>
+        /// Limpia la pantalla de los valoren anteriormente cargados 
+        /// </summary>
         private void Limpiar()
         {
             txtNumero1.Clear();
