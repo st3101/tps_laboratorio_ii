99998ac4cc8d3e562fc77d5142e7836bf4adb2a5
Santiago Leonardi <Santiago.t.leonardi@gmail.com>
2022-06-16 00:57:16 -0300

    Agregando tp2

---
+++ TrabajoPractico2/Entidades/Ciclomotor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color) : base (chasis,marca,color)
+        {
+
+        }
+
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+
+        protected override ETamanio Tamanio => ETamanio.Chico;
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio.ToString()}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
